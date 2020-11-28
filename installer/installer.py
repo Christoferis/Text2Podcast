bbdede9ebfcd4848f5a81850ba1167b6d67dd096
Christoferis <dale.breuer@gmx.de>
2020-11-28 19:04:49 +0100

    Rewritten Installer

--- installer/installer.py
+++ installer/installer.py
@@ -12,68 +12,50 @@ PO Box 1866, Mountain View, CA 94042, USA.
 
 '''
 
-from os import startfile, system, mkdir
-from time import sleep
+from tkinter import Tk, Label, Entry, Button, Frame
+from tkinter.filedialog import askdirectory as diropen
+from os import fspath, mkdir, system
+
+#Instance window
+main = Tk()
+main.title("Install Text2Podcast")
 
 
+#Main Install GUI
+def GUI():
+    global main
 
-
-#main Install function
-def yesno(msg):
-    #loop over until final answer
-    while True:
-        yn =  input(msg + "(y/n)?: ")
-
-        if yn in ("y", "Y"):
-            break
-        #no
-        elif yn in ("n", "N"):
-            print("Closing in 10 seconds")
-            sleep(10)
-            exit()
-        #different
-        else:
-            print("y/n please")
-            pass
-
-    return bool(True)
-
-
-
-def directory(path):
-    #make main dir
-    mkdir(path, "\Christoferis")
-    mainpath = path + "\Christoferis"
-
-    #make temp path
-    mkdir(path)
-    pass
-
-
-
-def start():
-    print("Welcome to the Text2Podcast Installer by Christoferis")
-    print("This installs the Text2Podcast to your Computer \n")
-    print("To start the installation, please install the following Software first (Follow Installation Details on Github or the respective Software Pages): ")
-    print("FFMPEG: http://ffmpeg.org/download.html")
-    print("Google Tesseract OCR: https://github.com/UB-Mannheim/tesseract/wiki\n")
-    print("Locate the Install location of the installed software and add them to the 'PATH' Variable")
+    #How to Label
+    Label(main, text="Welcome! This is an Installer which will install Text2Podcast on your PC").pack()
+    Label(main, text="First install Google Tesseract and FFMPEG on your Computer then copy the paths into the designated Textboxes.").pack()
+    Label(main, text="Then type in your desired Install path in the designated Textbox").pack()
+    Label(main, text="A detailed installation Tutorial for Windows and Linux can be found at https://sites.google.com/view/christoferis/code-projects/text2podcast").pack()
+    Label(main, text="FFMPEG Download: http://ffmpeg.org/download.html, Tesseract: https://tesseract-ocr.github.io/tessdoc/Installation.html ").pack()
     
-    if yesno("Type 'y' if you're finished installing and to proceed installing"):
-        print("License Agreement (CC BY-SA 4.0) \n" + LICENSE)
-        installpath = 'C:\Users\Public\Documents'
+    #Inputs
+    #Tesseract Path
+    Label(main, text="\nPath to Tesseract Binaries").pack()
+    tesspath = Entry(main)
+    tesspath.insert(index=0, string="C:\Program Files\Tesseract-OCR")
+    tesspath.pack(fill="x")
 
-        if yesno("Agree License Agreement? : "):
-            install = input("\n Where should Text2Podcast be installed? (CURRENT: " + installpath + " ) enter 0 to install to default path: ")
+    #FFMPEG Path
+    Label(main, text="\nPath to FFMPEG Binaries").pack()
+    ffmpath = Entry(main)
+    ffmpath.insert(index=0, string="C:\Program Files\FFMPEG")
+    ffmpath.pack(fill="x")
 
-            #Default Path thingy
-            if install != 0:
-                installpath = install
-                directory(installpath)
+    #installation path
+    Label(main, text="\nInstallpath").pack()
+    installpath = Entry(main)
+    installpath.insert(index=0, string=r"C:\Users\Public\documents\Christoferis\Text2Podcast")
+    installpath.pack(fill="x")
 
+    #start Button
+    
 
+#instance
+GUI()
 
-
-        
-#main Buffer
-start()
+#mainloop
+main.mainloop()
